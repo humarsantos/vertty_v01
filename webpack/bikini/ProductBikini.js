f5debbdfe9a07450312bc0858b469be41c314d31
Humar <santos_live@hotmail.com>
2017-04-23 17:23:00 +0100

    INIT: ProductBikini.js refactoring

--- webpack/bikini/ProductBikini.js
+++ webpack/bikini/ProductBikini.js
@@ -1,49 +1,55 @@
 var productBikini = {
-    init: function (product) {
+    init: function (t) {
         var e = this,
-            i = void 0 == product ? "" : product.handle.toLowerCase();
+            i = void 0 == t ? "" : t.handle.toLowerCase(),
+            n = {
+                green: "linear-gradient(#1d8782, #34b9b2, #34b9b2, #2bb4ac, #6cccc6)",
+                pink: "linear-gradient(#9d6072, #be7e90, #be7e90, #b16a7e, #c995a7)"
+            };
+            
+        this.mainCont = document.getElementsByClassName("main-featured")[0];
+        this.close = document.getElementsByClassName("close");
+        this.top = document.getElementById("top");
+        this.briefs = document.getElementById("briefs");
+        this.productContainer = document.getElementsByClassName("product")[0];
+        this.imgPreview = document.getElementsByClassName("col w-50 preview")[0].getElementsByTagName("img")[0];
+        this.product = t;
+        this.loading = $("#loader-operations, #overlay-operations");
+        this.descripTemporary = document.createElement("div");
+
+        if (this.product === undefined) return false;
         
-        var n = {
-            green: "linear-gradient(#1d8782, #34b9b2, #34b9b2, #2bb4ac, #6cccc6)",
-            pink: "linear-gradient(#9d6072, #be7e90, #be7e90, #b16a7e, #c995a7)"
-        };
-        
-        if (this.mainCont = document.getElementsByClassName("main-featured")[0],
-            this.close = document.getElementsByClassName("close"),
-            this.top = document.getElementById("top"),
-            this.briefs = document.getElementById("briefs"),
-            this.productContainer = document.getElementsByClassName("product")[0],
-            this.imgPreview = document.getElementsByClassName("col w-50 preview")[0].getElementsByTagName("img")[0],
-            this.product = name,
-            this.loading = $("#loader-operations, #overlay-operations"),
-            this.descripTemporary = document.createElement("div"),
-            void 0 === this.product) return !1;
-        
-        this.product.handle.indexOf("-") > -1 ? i = this.product.handle.split("-")[0] : i,
-        this.bikiniType = document.getElementsByClassName(i),
-        void 0 == this.product.description ? void 0 : this.descripTemporary.innerHTML = this.product.description;
+        this.product.handle.indexOf("-") > -1 ? i = this.product.handle.split("-")[0] : i;
+        this.bikiniType = document.getElementsByClassName(i);
+        this.product.description === undefined ? undefined : this.descripTemporary.innerHTML = this.product.description;
         
         var s = this.bikiniType[0].getAttribute("data-color").toLowerCase(),
             o = document.getElementById("prod-description"),
             a = this.descripTemporary.getElementsByClassName(s)[0].innerText;
         
-        void 0 !== o && (o.innerHTML = this.descripTemporary.getElementsByClassName(s)[0].innerText);
+        o !== undefined && (o.innerHTML = this.descripTemporary.getElementsByClassName(s)[0].innerText);
         
         var r = this.imgPreview.src.indexOf("_p") > -1 ? "pink" : "green";
         
         i.split("-")[0];
         
-        this.productContainer.style.backgroundImage = n[r], window.addEventListener("load", function () {
+        this.productContainer.style.backgroundImage = n[r];
+        
+        window.addEventListener("load", function () {
             e.createDetails()
         });
         
         for (var l = 0; l < this.close.length; l++) this.close[l].onclick = function (t) {
+            
             var e = this.parentElement;
+            
             if (e.className.indexOf("disabled") < 0) {
                 var i = $(".fancybox.top", ".img-details"),
                     n = $(".fancybox.bottom", ".img-details"),
                     s = $(".other", ".img-details");
-                e.className = "disabled", void 0 !== i[0] ? "top" == this.parentElement.id ? (i.removeClass("fancybox").addClass("disabled"), i.click(function (t) {
+                
+                e.className = "disabled",
+                i[0] !== undefined ? "top" == this.parentElement.id ? (i.removeClass("fancybox").addClass("disabled"), i.click(function (t) {
                     t.preventDefault()
                 })) : (n.removeClass("fancybox").addClass("disabled"), n.click(function (t) {
                     t.preventDefault()
@@ -54,25 +60,27 @@ var productBikini = {
                 var i = $(".top", ".img-details"),
                     n = $(".bottom", ".img-details"),
                     s = $(".other", ".img-details");
-                e.className = e.className.replace("disabled", ""), void 0 !== i[0] ? "top" == this.parentElement.id ? (i.removeClass("disabled").addClass("fancybox"), i.click(function (t) {
-                    return !0
+                
+                e.className = e.className.replace("disabled", ""), i[0] !== undefined ? "top" == this.parentElement.id ? (i.removeClass("disabled").addClass("fancybox"), i.click(function (t) {
+                    return true;
                 })) : (n.removeClass("disabled").addClass("fancybox"), n.click(function (t) {
-                    return !0
+                    return true;
                 })) : (s.removeClass("disabled").addClass("fancybox"), s.click(function (t) {
                     t.preventDefault()
                 }))
             }
-            return !1
+            return false;
         };
-        
         for (var h = 0; h < this.bikiniType.length; h++)
-            
-            this.bikiniType[h].pos = h, this.bikiniType[h].onclick = function (t) {
+            this.bikiniType[h].pos = h,
+            this.bikiniType[h].onclick = function (t) {
                 
-                var i, s = this.getAttribute("data-color").toLowerCase(),
+                var i,
+                    s = this.getAttribute("data-color").toLowerCase(),
                     r = function (t) {
-                        for (var i = 0; i < e.product.variants.length; i++)
-                            if (e.product.variants[i].title.toLowerCase() == t)
+                        for (var i = 0; i < e.product.variants.length; i++) {                            
+                            if (e.product.variants[i].title.toLowerCase() == t) {
+                                
                                 if (e.product.variants[i].available !== !0) {
                                     var n = document.getElementById("purchase-button");
                                     n.className = "solded-out"
@@ -80,17 +88,26 @@ var productBikini = {
                                     var n = document.getElementById("purchase-button");
                                     n.className.indexOf("solded-out") > -1 && (n.className = n.className.replace("solded-out", "add-cart"))
                                 }
+                            }
+                        }
                     };
-                if (r(s), void 0 !== o && (a = e.descripTemporary.getElementsByClassName(s)[0].innerText), !(this.className.indexOf("active") < 0)) return !1;
+                
+                if (r(s), void 0 !== o && (a = e.descripTemporary.getElementsByClassName(s)[0].innerText), !(this.className.indexOf("active") < 0)) return false;
+                
                 e.loading.fadeIn();
+                
                 for (var l = 0; l < e.bikiniType.length; l++) {
                     var h = e.bikiniType[l].className.indexOf("active") > -1,
                         c = e.bikiniType[l].className.indexOf("briefs") > -1;
-                    h && (c && this.className.indexOf("briefs") > -1 && (e.bikiniType[l].className = e.bikiniType[l].className.replace(" active", "")), !c && this.className.indexOf("briefs") < 0 && (e.bikiniType[l].className = e.bikiniType[l].className.replace(" active", "")))
-                }
+                    
+                    h && (c && this.className.indexOf("briefs") > -1 && (e.bikiniType[l].className = e.bikiniType[l].className.replace(" active", "")),
+                    !c && this.className.indexOf("briefs") < 0 && (e.bikiniType[l].className = e.bikiniType[l].className.replace(" active", "")))
+                }                
                 this.className = this.className + " active";
-                for (var u = 0; u < e.product.images.length; u++) {
+                
+                for (var u = 0; u < e.product.images.length; u++) {    
                     var d = e.product.handle;
+                    
                     if (e.product.images[u].indexOf(d + "-" + s) > -1) {
                         i = e.product.images[u];
                         var p = i.indexOf("_g") > -1 ? "green" : "pink";
@@ -119,14 +136,14 @@ var productBikini = {
         for (var r = 0; r < this.allDetails.length; r++) {
             var l = this.allDetails[r].href.indexOf("top") > -1 ? "top" : this.allDetails[r].href.indexOf("bottom") > -1 ? "bottom" : "other";
             switch (l) {
-            case "top":
-                this.allDetails[r].className = this.allDetails[r].className + " top";
-                break;
-            case "bottom":
-                this.allDetails[r].className = this.allDetails[r].className + " bottom";
-                break;
-            case "other":
-                this.allDetails[r].className = this.allDetails[r].className + " other"
+                case "top":
+                    this.allDetails[r].className = this.allDetails[r].className + " top";
+                    break;
+                case "bottom":
+                    this.allDetails[r].className = this.allDetails[r].className + " bottom";
+                    break;
+                case "other":
+                    this.allDetails[r].className = this.allDetails[r].className + " other"
             }
         }
         this.hideDetails(a), void 0 !== this.detailsCont && (this.detailsCont.style.height = this.detailsCont.children[0].offsetHeight + "px", this.detailsCont.style.top = (this.detailsCont.parentElement.offsetHeight - this.detailsCont.children[0].offsetHeight) / 2 + "px")
@@ -187,7 +204,8 @@ var productBikini = {
                     var currentItem = "Top / " + this.bikiniColour[0] + " / " + this.sizeName[0],
                         otherItem = "Brief / " + this.bikiniColour[1] + " / " + this.sizeName[1];
                     me.variantToAdd = [], getVariantId(currentItem, otherItem)
-                } else if ("active" == this.topStatus) {
+                }
+        else if ("active" == this.topStatus) {
             var currentItem = "Top / " + this.bikiniColour[0] + " / " + this.sizeName[0];
             getVariantId(currentItem)
         } else {
