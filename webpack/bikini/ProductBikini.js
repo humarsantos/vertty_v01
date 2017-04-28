179aba30d2812145a282c695052757c805d44b57
Humar <santos_live@hotmail.com>
2017-04-28 11:47:56 +0100

    UPD: Organizing files CHG: Unpacking some bundles because the bundles original files were lost.

--- webpack/bikini/ProductBikini.js
+++ webpack/bikini/ProductBikini.js
@@ -126,15 +126,26 @@ var productBikini = {
             e = document.getElementsByClassName("nav-towel"),
             i = e.length > 1 ? this.bikiniType.length / 2 : this.bikiniType.length,
             n = document.getElementsByClassName("preview")[0].getElementsByTagName("img")[0];
-        this.allDetails = document.getElementsByClassName("fancybox"), this.detailsCont = document.getElementsByClassName("img-details")[0], this.imgDetails = this.detailsCont.getElementsByClassName("fancybox");
+        
+        this.allDetails = document.getElementsByClassName("fancybox"),
+        this.detailsCont = document.getElementsByClassName("img-details")[0],
+        this.imgDetails = this.detailsCont.getElementsByClassName("fancybox");
+        
         for (var s = 0; s < i; s++) t.push(this.bikiniType[s].getAttribute("data-color").toLowerCase());
-        for (var o = 0; o < t.length; o++)
+        
+        for (var o = 0; o < t.length; o++) {
+            
             if (n.src.indexOf(t[o]) > -1) {
                 var a = t[o];
-                e.length > 1 ? ($(".bikini", $(".nav-towel")[0])[o].className = $(".bikini", $(".nav-towel")[0])[o].className + " active", $(".bikini", $(".nav-towel")[1])[o].className = $(".bikini", $(".nav-towel")[1])[o].className + " active") : $(".bikini")[o].className = $(".bikini")[o].className + " active"
+                e.length > 1 ? ($(".bikini", $(".nav-towel")[0])[o].className = $(".bikini", $(".nav-towel")[0])[o].className + " active",
+                                $(".bikini", $(".nav-towel")[1])[o].className = $(".bikini", $(".nav-towel")[1])[o].className + " active") : $(".bikini")[o].className = $(".bikini")[o].className + " active";
             }
+        }
+        
         for (var r = 0; r < this.allDetails.length; r++) {
+            
             var l = this.allDetails[r].href.indexOf("top") > -1 ? "top" : this.allDetails[r].href.indexOf("bottom") > -1 ? "bottom" : "other";
+            
             switch (l) {
                 case "top":
                     this.allDetails[r].className = this.allDetails[r].className + " top";
@@ -143,17 +154,19 @@ var productBikini = {
                     this.allDetails[r].className = this.allDetails[r].className + " bottom";
                     break;
                 case "other":
-                    this.allDetails[r].className = this.allDetails[r].className + " other"
+                    this.allDetails[r].className = this.allDetails[r].className + " other";
             }
         }
-        this.hideDetails(a), void 0 !== this.detailsCont && (this.detailsCont.style.height = this.detailsCont.children[0].offsetHeight + "px", this.detailsCont.style.top = (this.detailsCont.parentElement.offsetHeight - this.detailsCont.children[0].offsetHeight) / 2 + "px")
+        this.hideDetails(a);
+        this.detailsCont !== undefined && (this.detailsCont.style.height = this.detailsCont.children[0].offsetHeight + "px", this.detailsCont.style.top = (this.detailsCont.parentElement.offsetHeight - this.detailsCont.children[0].offsetHeight) / 2 + "px");
     },
     hideDetails: function (t) {
-        for (var e = 0; e < this.imgDetails.length; e++) this.imgDetails[e].getElementsByTagName("img")[0].src.indexOf(t) < 0 && (this.imgDetails[e].style.display = "none")
+        for (var e = 0; e < this.imgDetails.length; e++) {
+            this.imgDetails[e].getElementsByTagName("img")[0].src.indexOf(t) < 0 && (this.imgDetails[e].style.display = "none");
+        }
     },
     showDetails: function (t) {
         for (var e = [], i = ["top", "bottom"], n = 0; n < this.bikiniType.length; n++) this.bikiniType[n].className.indexOf("active") > -1 && e.push(this.bikiniType[n].getAttribute("data-color").toLowerCase());
-        console.log(e);
         for (var s = 0; s < this.imgDetails.length; s++) this.imgDetails[s].style.display = "none";
         for (var o = 0; o < this.imgDetails.length; o++)
             for (var a = 0; a < e.length; a++) this.imgDetails[o].className.indexOf("other") < 0 ? this.imgDetails[o].getElementsByTagName("img")[0].src.indexOf(e[a]) > -1 && this.imgDetails[o].className.indexOf(i[a]) > -1 && (this.imgDetails[o].style.display = "block") : this.imgDetails[o].getElementsByTagName("img")[0].src.indexOf(e[a]) > -1 && (this.imgDetails[o].style.display = "block");
@@ -163,22 +176,31 @@ var productBikini = {
         function getVariantId(t, e) {
             for (var i = 0; i < me.variants.length; i++) void 0 !== e ? me.variants[i].title == t ? me.variantToAdd.push(me.variants[i].id) : me.variants[i].title == e && me.variantToAdd.push(me.variants[i].id) : (me.variants[i].title == t || t.indexOf(me.variants[i].title) > -1) && (me.variantToAdd = me.variants[i].id)
         }
-        this.topStatus = this.top.className.indexOf("disabled") < 0 ? "active" : "disabled", "trykini" !== this.product.handle && (this.briefsStatus = this.briefs.className.indexOf("disabled") < 0 ? "active" : "disabled"), this.sizesContainer = document.getElementsByClassName("sizes"), this.sizeName = [];
+        this.topStatus = this.top.className.indexOf("disabled") < 0 ? "active" : "disabled",
+        this.product.handle !== "trykini" && (this.briefsStatus = this.briefs.className.indexOf("disabled") < 0 ? "active" : "disabled"),
+        this.sizesContainer = document.getElementsByClassName("sizes"), this.sizeName = [];
+        
         var me = this,
             count = 0;
+        
         this.applyModal = function (t, e, i, n, s, o) {
             return this.title = "Towel" == t ? "Beach Towel" : t, this.price = e, this.subtotal = i, this.variant = s, this.size = void 0 == o ? "" : o, this.image = n, this.successTowel = "<div class='vertty-order-modal'><div class='order-img'><img src='" + this.image + "'></div><div class='order-info'><span class='tlt'>" + this.title + "</span><span>Qty:  <b>1</b></span><span>TP:  <b>" + this.variant + "</b></span><span class='item-price'>" + this.price + "</span></div><div class='order-price'><span>Subtotal <span class='total-value'>" + this.subtotal + "</span></span></div></div><div class='btn-box' style='text-align: right;'><a href='/cart' class='view-cart'></a></div>", this.successTowel
         };
+        
         for (var j = 0; j < this.sizesContainer.length; j++) {
             this.sizesContainer[j].sizes = this.sizesContainer[j].getElementsByTagName("LI");
+            
             for (var b = 0; b < this.sizesContainer[j].sizes.length; b++)
-                if (this.sizesContainer[j].sizes[b].className.indexOf("active") < 0 ? count++ : this.sizeName.push(this.sizesContainer[j].sizes[b].getElementsByTagName("a")[0].innerText), count == this.sizesContainer[j].sizes.length * this.sizesContainer.length) return swal({
-                    title: "Warning!",
-                    text: "Select your product size",
-                    type: "warning",
-                    allowOutsideClick: !0,
-                    customClass: "sweet-old"
-                }), !1
+                
+                if (this.sizesContainer[j].sizes[b].className.indexOf("active") < 0 ? count++ : this.sizeName.push(this.sizesContainer[j].sizes[b].getElementsByTagName("a")[0].innerText),
+                    count == this.sizesContainer[j].sizes.length * this.sizesContainer.length)
+                    return swal({
+                        title: "Warning!",
+                        text: "Select your product size",
+                        type: "warning",
+                        allowOutsideClick: !0,
+                        customClass: "sweet-old"
+                    }), !1
         }
         this.bikiniColour = [], this.variants = this.product.variants, this.variantToAdd = null, this.cartCounter = document.getElementsByClassName("cart-count");
         for (var m = 0; m < this.bikiniType.length; m++) this.bikiniType[m].className.indexOf("active") > -1 && this.bikiniType[m].className.indexOf("briefs") > -1 ? this.bikiniColour.push(this.bikiniType[m].getAttribute("data-color")) : this.bikiniType[m].className.indexOf("active") > -1 && this.bikiniType[m].className.indexOf("briefs") < 0 && this.bikiniColour.push(this.bikiniType[m].getAttribute("data-color"));
